6e03e0ca7cedfc4f1809690d5cf8e8e1f612c090
chinmaii <chinmaiiswami@gmail.com>
2024-03-12 18:52:06 +0530

    datatypes done

---
+++ 1-Basics/1_variables.js
@@ -0,0 +1,12 @@
+const accountId = 144553;
+let accountEmail = "chinmaii@gmail.com";
+var accountPassword = "12345";
+accountCity = "jaipur";
+
+accountEmail = "hc@ab.com";
+accountPassword = "565435";
+accountCity = "Jaipur";
+
+console.table = ([accountEmail, accountPassword, accountCity]);
+
+/*prefer not to use var because of issue in block and functional scope*/
---
+++ 1-Basics/2_dataTypes.js
@@ -0,0 +1,21 @@
+"use strict"; // treat all JS code as newer version
+
+// we are using nodejs, not browser
+// code readability is imp
+console.log(3 + 3);
+console.log("Chinmai");
+
+//tc 39 - ECMA JS site
+
+let name = "chinmai"
+let age = 18
+let isLoggedIn = false
+let state ;
+
+//number
+//bigint
+//string
+//boolean
+//null = standalone value
+//undefined
+// symbol
